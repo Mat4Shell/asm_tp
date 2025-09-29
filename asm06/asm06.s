80aacdcc861fca4a6f89017443cf4f165da801cc
Mat4Shell <mathias_emon@yahoo.com>
2025-09-29 13:56:17 +0200

    add06

--- asm06/asm06.s
+++ asm06/asm06.s
@@ -1,77 +1,133 @@
-section .data
-    	msg_format db "%d", 10, 0
-
-section .bss
-    	arg1 resq 1
-    	arg2 resq 1
-    	sum resq 1
-
-section .text
-    	global _start
-
+global _start
 _start:
-    	pop rax
-    	cmp rax, 3
-    	jne error
+section .bss
+    result resb 20
+section .text
+    mov rsi, [rsp + 16]
+    cmp rsi, 0
+    je error
 
-    	pop rsi
-    	pop rax
-    	mov [arg1], rax
-    	pop rax
-    	mov [arg2], rax
-
-    	mov rax, 0
-    	mov rcx, 0
-convert_arg1:
-    	mov rdx, 10
-    	mov rbx, [arg1]
-    	movzx rbx, byte [rbx + rcx]
-    	cmp rbx, 0
-    	je end_convert_arg1
-    	sub rbx, '0'
-    	imul rax, rdx
-    	add rax, rbx
-    	inc rcx
-    	jmp convert_arg1
-
-end_convert_arg1:
-    	mov [arg1], rax
-
-    	mov rax, 0
-    	mov rcx, 0
-convert_arg2:
-    	mov rdx, 10
-    	mov rbx, [arg2]
-    	movzx rbx, byte [rbx + rcx]
-    	cmp rbx, 0
-    	je end_convert_arg2
-    	sub rbx, '0'
-    	imul rax, rdx
-    	add rax, rbx
-    	inc rcx
-    	jmp convert_arg2
-
-end_convert_arg2:
-    	mov [arg2], rax
-
-    	mov rax, [arg1]
-    	add rax, [arg2]
-    	mov [sum], rax
-
-	mov rdi, msg_format
-    	mov rsi, [sum]
-    	mov rax, 0
-    	call printf
+    mov rdi, [rsp + 24]
+    cmp rdi, 0
+    je error
 
 
-    	mov rax, 60
-    	xor rdi, rdi
-    	syscall
+
+convert_to_int:
+    movzx rbx, byte[rsi + rcx]
+    test rbx, rbx
+    jz second_arg
+    cmp rbx, 10
+    je second_arg
+
+    cmp rbx, 0x2d
+    je boolean
+
+    cmp rbx, 0x30
+    jb is_letter
+    cmp rbx, 0x39
+    ja is_letter 
+
+    sub rbx, '0'
+    imul rax, rax, 10
+    add rax, rbx
+    inc rcx
+    jmp convert_to_int
+
+second_arg:
+    test r11, r11
+    jz no_neg
+    neg rax
+
+no_neg:
+    mov rbx, rax
+    xor rax, rax
+    xor rcx, rcx
+
+convert_to_int2:
+    movzx r10, byte[rdi + rcx]
+    test r10, r10
+    jz addition
+    cmp r10, 10
+    je addition
+
+    cmp r10, 0x2d
+    je boolean2
+
+    cmp r10, 0x30
+    jb is_letter
+    cmp r10, 0x39
+    ja is_letter 
+
+    sub r10, '0'
+    imul rax, rax, 10
+    add rax, r10
+    inc rcx
+    jmp convert_to_int2
+
+addition:
+    add rax, rbx
+    ;add \0 at the end of the buffer
+    mov rsi, result
+    add rsi, 19
+    mov byte[rsi], 0
+    cmp r11, 1
+    neg rax
+
+    jmp convert_to_string
+
+convert_to_string:
+    xor rdx, rdx
+    mov rbx, 10
+    div rbx
+    add dl, '0'
+    dec rsi
+    mov byte[rsi], dl
+    test rax, rax
+    jnz convert_to_string
+
+    mov rax, 1
+    mov rdi, 1
+    mov rdx, 20
+    syscall
+
+; strlen:
+;     cmp byte[rsi + rcx], 0
+;     je print
+;     inc rcx
+;     jmp strlen
+
+exit:
+    xor rax, rax
+    xor rdi, rdi
+
+    mov rax, 60
+    xor rdi, rdi
+    syscall
 
 error:
-    	mov rax, 60
-    	xor rdi, rdi
-    	syscall
+    xor rax, rax
+    xor rdi, rdi
 
-section .text
-    	extern printf
+    mov rax, 60
+    mov rdi, 1
+    syscall
+
+boolean:
+    mov r11, 1
+    neg rbx
+    jmp convert_to_int2
+
+boolean2:
+    mov r12, 1
+    neg r10
+    jmp addition
+
+
+is_letter:
+    xor rax, rax
+    xor rdi, rdi
+
+    mov rax, 60
+    mov rdi, 2
+    syscall
