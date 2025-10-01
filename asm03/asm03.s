896b20b16de5472658a811ff885a0bc02c78026d
Mat4Shell <mathias_emon@yahoo.com>
2025-10-01 11:13:32 +0200

    mod03

--- asm03/asm03.s
+++ asm03/asm03.s
@@ -1,77 +1,50 @@
+global _start
+_start:
+
 section .data
-    	value db "42", 0
-    	msg db "1337", 0
+	msg db "1337", 10
 
 section .bss
-    	arg1 resq 1
-    	size_arg1 resq 1
+        input resb 6
 
 section .text
-    	global _start
+	
+	mov rax, [rsp]
+	cmp rax, 2
+	jl not_42
 
-_start:
-    	pop rax
-    	cmp rax, 2
-    	jne error
+	mov rdi, [rsp + 16]
 
-    	pop rsi
-    	pop rax
-   	mov [arg1], rax
+	mov al, byte [rdi]
+	cmp al, '4'
+	jne not_42
 
-    	mov rcx, 0
-size1:
-    	mov rax, [arg1]
-    	cmp byte [rax + rcx], 0
-    	je end_size1
-    	inc rcx
-    	jmp size1
+ 	mov al, byte [rdi + 1]
+ 	cmp al, '2'
+ 	jne not_42
 
-end_size1:
-    	mov [size_arg1], rcx
+ 	mov al, byte [rdi + 2]
+ 	cmp al, 0
+ 	jne not_42
 
-    	mov rax, 0
-    	mov rcx, 0
-convert_loop:
-    	mov rdx, 10
-    	mov rbx, [arg1]
-    	movzx rbx, byte [rbx + rcx]
-    	cmp rbx, 0
-    	je end_convert
-    	sub rbx, '0'
-   	imul rax, rdx
- 	add rax, rbx
-	inc rcx
- 	jmp convert_loop
+	xor rax, rax
+	xor rdi, rdi
+ 	xor rsi, rsi    
 
-end_convert:
-	cmp rax, 42
-	je is_42
-	mov rax, 1
-	jmp end_prog
-
-is_42:
-    	mov rax, 1
-    	mov rdi, 1
-    	mov rsi, msg
-    	mov rdx, 4
-    	syscall
-
-    	mov rax, 60
-	mov rdi, 0
-    	syscall
-
-end_prog:
-	mov rax, 1
+ 	mov rax, 1
 	mov rdi, 1
 	mov rsi, msg
-	mov rdx, 4
+	mov rdx, 10
 	syscall
 
 	mov rax, 60
-	mov rdi, 1
+	mov rdi, 0
 	syscall
 
-error:
-	mov rax, 60
-	xor rdi, rdi
-	syscall
+not_42:
+ 	xor rax, rax
+    	xor rdi, rdi    
+
+   	mov rax, 60
+    	mov rdi, 1
+    	syscall
