a17b264908f6936e0c4434a0da80766844a6fe84
Mat4Shell <mathias_emon@yahoo.com>
2025-10-01 11:07:31 +0200

    mod02

--- asm02/asm02.s
+++ asm02/asm02.s
@@ -1,50 +1,49 @@
 global _start
 _start:
 
-section .data
-    text db '1337', 10
-
 section .bss
-    input resb 4
-        
-section .text
+        input resb 6
 
-    mov rax, 0
-    mov rdi, 0
-    mov rsi, input
-    mov rdx, 4
-    syscall
+section .data
+	msg db "1337", 10
+
+section .text
+	
+	mov rax, 0
+	mov rdi, 0
+	mov rsi, input
+	mov rdx, 32
+	syscall
 
     mov al, byte [input]
     cmp al, '4'
-    jne not_equal
+    jne not_42
 
     mov al, byte [input + 1]
     cmp al, '2'
-    jne not_equal
+    jne not_42
 
     mov al, byte [input + 2]
     cmp al, 10
-    jne not_equal
+    jne not_42
 
     xor rax, rax
     xor rdi, rdi
-    xor rsi, rsi
-    
+    xor rsi, rsi    
+
     mov rax, 1
     mov rdi, 1
-    mov rsi, text
-    mov rdx, 10
+    mov rsi, msg
+    mov rdi, 4
     syscall
 
     mov rax, 60
-    xor rdi, rdi
-    xor rsi, rsi
+    mov rdi, 0
     syscall
 
-not_equal:
+not_42:
     xor rax, rax
-    xor rdi, rdi
+    xor rdi, rdi    
 
     mov rax, 60
     mov rdi, 1
