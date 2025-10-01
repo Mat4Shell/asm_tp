ae03dc5d59be246253a5502a899448bd3f7c5ef3
Mat4Shell <mathias_emon@yahoo.com>
2025-10-01 11:20:57 +0200

    mod04

--- asm04/asm04.s
+++ asm04/asm04.s
@@ -11,17 +11,40 @@ _start:
 	mov rdx, 6
 	syscall
 
-   	mov ax, [input]
-   	and ax, 1
-   	jz evnn
-   	jmp outprog
+loop:
+	movzx rbx, byt [input + rcx]
+	test rbx, rbx
+	jz converted
+
+	cmp rbx, 10
+	je converted
+
+	cmp rbx, '0'
+	jb lettr
+	cmp rbx, '9'
+	ja lettr
+
+	sub rbx, '0'
+	imul rax, rax, 10
+	add rax, rbx
+	inc rcx
+	jmp converted
+
+converted:
+	and rax, 1
+	jz evnn
+
+outprog:
+	mov rax, 60
+	mov rdi, 1
+	syscall
 
 evnn:
    	mov rax, 60
 	mov rdi, 0
 	syscall
 
-outprog:
+lettr:
 	mov rax, 60
-	mov rdi, 1
+	mov rdi, 2
 	syscall
