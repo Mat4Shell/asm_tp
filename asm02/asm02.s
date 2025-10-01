26a0b35e7df2412c03ba9261aca8a08e656af1cd
Mat4Shell <51493013+Mat4Shell@users.noreply.github.com>
2025-10-01 10:56:17 +0200

    mod asm02

--- asm02/asm02.s
+++ asm02/asm02.s
@@ -1,5 +1,5 @@
 section .bss
-        input resb 6
+        input resb 4
 
 section .data
         msg db "1337", 10
@@ -11,39 +11,39 @@ _start:
 	mov rax, 0
 	mov rdi, 0
 	mov rsi, input
-	mov rdx, 32
+	mov rdx, 4
 	syscall
 
-        mov al, byte [input]
-        cmp al, '4'
-        jne not_42
+    mov al, byte [input]
+    cmp al, '4'
+    jne not_42
 
-        mov al, byte [input + 1]
-        cmp al, '2'
-        jne not_42
+	mov al, byte [input + 1]
+    cmp al, '2'
+    jne not_42
 
-        mov al, byte [input + 2]
-        cmp al, 10
-        jne not_42
+    mov al, byte [input + 2]
+    cmp al, 10
+    jne not_42
 
 	xor rax, rax
 	xor rdi, rdi
 	xor rsi, rsi	
 
-        mov rax, 1
-        mov rdi, 1
-        mov rsi, msg
-        mov rdi, 4
-        syscall
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, msg
+    mov rdi, 10
+    syscall
 
-        mov rax, 60
-        mov rdi, 0
-        syscall
+    mov rax, 60
+    mov rdi, 0
+    syscall
 
 not_42:
 	xor rax, rax
 	xor rdi, rdi	
 
-        mov rax, 60
-        mov rdi, 1
-        syscall
+    mov rax, 60
+    mov rdi, 1
+    syscall
