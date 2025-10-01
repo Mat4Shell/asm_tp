616f603b2f78584749e28d0c161ef8ed90cb4963
Mat4Shell <mathias_emon@yahoo.com>
2025-10-01 11:47:28 +0200

    mod04

--- asm06/asm06.s
+++ asm06/asm06.s
@@ -1,133 +1,147 @@
 global _start
-_start:
+
 section .bss
-    result resb 20
+    result resb 20          ; Buffer pour le résultat
+
 section .text
-    mov rsi, [rsp + 16]
+_start:
+    ; Charger les paramètres dans rsi (premier argument) et rdi (second argument)
+    mov rsi, [rsp + 16]      ; Premier argument
     cmp rsi, 0
     je error
 
-    mov rdi, [rsp + 24]
+    mov rdi, [rsp + 24]      ; Deuxième argument
     cmp rdi, 0
     je error
 
+    ; Initialiser les registres
+    xor rax, rax             ; Effacer rax (pour le premier entier)
+    xor rbx, rbx             ; Effacer rbx (pour le deuxième entier)
+    xor rcx, rcx             ; Compteur pour les boucles
+    xor r11, r11             ; Signe du premier nombre (0 = positif, 1 = négatif)
+    xor r12, r12             ; Signe du deuxième nombre
 
-
+; Convertir le premier argument en entier
 convert_to_int:
-    movzx rbx, byte[rsi + rcx]
-    test rbx, rbx
+    movzx r10, byte [rsi + rcx]  ; Charger le caractère actuel
+    
+    test r10, r10                ; Vérifier si c'est la fin de la chaîne
     jz second_arg
-    cmp rbx, 10
+    cmp r10, 10
     je second_arg
 
-    cmp rbx, 0x2d
-    je boolean
-
-    cmp rbx, 0x30
+   
+    cmp r10, '0'
     jb is_letter
-    cmp rbx, 0x39
+    cmp r10, '9'
     ja is_letter 
 
-    sub rbx, '0'
-    imul rax, rax, 10
-    add rax, rbx
-    inc rcx
+    sub r10, '0'                 ; Convertir ASCII en entier
+    imul rax, rax, 10            ; Décaler à gauche
+    add rax, r10                 ; Ajouter le chiffre converti
+    inc rcx                      ; Passer au caractère suivant
+    jmp convert_to_int
+
+negative_first_arg:
+    mov r11, 1                   ; Indiquer que le nombre est négatif
+    inc rcx                      ; Passer au caractère suivant
     jmp convert_to_int
 
 second_arg:
-    test r11, r11
-    jz no_neg
-    neg rax
+    ; Appliquer le signe du premier nombre
+    test r11, r11                ; Vérifier le signe du premier nombre
+    jz no_neg_first_arg
+    neg rax                      ; Rendre le premier nombre négatif
 
-no_neg:
-    mov rbx, rax
-    xor rax, rax
-    xor rcx, rcx
+no_neg_first_arg:
+    mov rbx, rax                 ; Stocker le premier entier dans rbx
+    xor rax, rax                 ; Réinitialiser rax pour le deuxième entier
+    xor rcx, rcx                 ; Réinitialiser le compteur
+    xor r10, r10                 ; Réinitialiser r10 pour le deuxième argument
 
+; Convertir le deuxième argument en entier
 convert_to_int2:
-    movzx r10, byte[rdi + rcx]
-    test r10, r10
+    movzx r10, byte [rdi + rcx]  ; Charger le caractère actuel
+    test r10, r10                ; Vérifier si c'est la fin de la chaîne
     jz addition
     cmp r10, 10
     je addition
 
-    cmp r10, 0x2d
-    je boolean2
+    cmp r10, '-'                 ; Vérifier le signe '-'
+    je negative_second_arg
 
-    cmp r10, 0x30
+    cmp r10, '0'
     jb is_letter
-    cmp r10, 0x39
+    cmp r10, '9'
     ja is_letter 
 
-    sub r10, '0'
-    imul rax, rax, 10
-    add rax, r10
-    inc rcx
+    sub r10, '0'                 ; Convertir ASCII en entier
+    imul rax, rax, 10            ; Décaler à gauche
+    add rax, r10                 ; Ajouter le chiffre converti
+    inc rcx                      ; Passer au caractère suivant
+    jmp convert_to_int2
+
+negative_second_arg:
+    mov r12, 1                   ; Indiquer que le nombre est négatif
+    inc rcx                      ; Passer au caractère suivant
     jmp convert_to_int2
 
 addition:
-    add rax, rbx
-    ;add \0 at the end of the buffer
-    mov rsi, result
-    add rsi, 19
-    mov byte[rsi], 0
-    cmp r11, 1
-    neg rax
+    ; Appliquer le signe au deuxième nombre
+    test r12, r12                ; Vérifier le signe du deuxième nombre
+    jz no_neg_second_arg
+    neg rax                      ; Rendre le deuxième nombre négatif
 
-    jmp convert_to_string
+no_neg_second_arg:
+    add rax, rbx                 ; Additionner les deux entiers
+
+    ; Préparer pour convertir le résultat en chaîne
+    mov rsi, result              ; Pointeur vers le buffer
+    add rsi, 19                  ; Placer le pointeur à la fin du buffer
+    mov byte [rsi], 0            ; Ajouter un terminateur nul
+
+    ; Gérer le cas où le résultat est négatif
+    test rax, rax
+    jns convert_to_string        ; Si le résultat est positif, sauter
+    neg rax                      ; Rendre le résultat positif pour la conversion
+    
+    dec rsi                      ; Préparer l'espace pour le signe
+    mov byte [rsi], '-'          ; Ajouter le signe '-' dans le buffer
+    dec rsi              
 
 convert_to_string:
-    xor rdx, rdx
+    xor rdx, rdx                 ; Effacer rdx avant la division
     mov rbx, 10
-    div rbx
-    add dl, '0'
-    dec rsi
-    mov byte[rsi], dl
-    test rax, rax
-    jnz convert_to_string
+next_digit:
+    xor rdx, rdx                 ; Effacer rdx avant la division
+    div rbx                      ; Diviser rax par 10
+    add dl, '0'                  ; Convertir le reste en ASCII
+    mov byte [rsi], dl           ; Stocker le caractère dans le buffer
+    dec rsi                      ; Déplacer le pointeur
+    test rax, rax                ; Vérifier si le quotient est 0
+    jnz next_digit
 
-    mov rax, 1
-    mov rdi, 1
-    mov rdx, 20
+    ; Ajuster rsi pour pointer au début de la chaîne
+    inc rsi
+
+    ; Afficher la chaîne résultante
+    mov rax, 1                   ; syscall: sys_write
+    mov rdi, 1                   ; file descriptor: stdout
+    mov rdx, 20                  ; Longueur de la chaîne
     syscall
-
-; strlen:
-;     cmp byte[rsi + rcx], 0
-;     je print
-;     inc rcx
-;     jmp strlen
 
 exit:
     xor rax, rax
     xor rdi, rdi
-
-    mov rax, 60
-    xor rdi, rdi
+    mov rax, 60                  ; syscall: sys_exit
     syscall
 
 error:
-    xor rax, rax
-    xor rdi, rdi
-
     mov rax, 60
-    mov rdi, 1
+    mov rdi, 1                   ; Code d'erreur pour argument manquant
     syscall
 
-boolean:
-    mov r11, 1
-    neg rbx
-    jmp convert_to_int2
-
-boolean2:
-    mov r12, 1
-    neg r10
-    jmp addition
-
-
 is_letter:
-    xor rax, rax
-    xor rdi, rdi
-
     mov rax, 60
-    mov rdi, 2
+    mov rdi, 2                   ; Code d'erreur pour argument non valide
     syscall
