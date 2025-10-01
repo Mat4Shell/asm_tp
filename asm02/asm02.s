99d6793add8ba35df4c1865e1d879c5077ece070
Mat4Shell <mathias_emon@yahoo.com>
2025-10-01 11:03:52 +0200

    test02

--- asm02/asm02.s
+++ asm02/asm02.s
@@ -1,49 +1,51 @@
-section .bss
-        input resb 6
+global _start
+_start:
 
 section .data
-        msg db "1337", 10
+    text db '1337', 10
 
+section .bss
+    input resb 4
+        
 section .text
-global _start
 
-_start:
-	mov rax, 0
-	mov rdi, 0
-	mov rsi, input
-	mov rdx, 32
-	syscall
+    mov rax, 0
+    mov rdi, 0
+    mov rsi, input
+    mov rdx, 4
+    syscall
 
-        mov al, byte [input]
-        cmp al, '4'
-        jne not_42
+    mov al, byte [input]
+    cmp al, '4'
+    jne not_equal
 
-        mov al, byte [input + 1]
-        cmp al, '2'
-        jne not_42
+    mov al, byte [input + 1]
+    cmp al, '2'
+    jne not_equal
 
-        mov al, byte [input + 2]
-        cmp al, 10
-        jne not_42
+    mov al, byte [input + 2]
+    cmp al, 10
+    jne not_equal
 
-	xor rax, rax
-	xor rdi, rdi
-	xor rsi, rsi	
+    xor rax, rax
+    xor rdi, rdi
+    xor rsi, rsi
+    
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, text
+    mov rdx, 10
+    syscall
 
-        mov rax, 1
-        mov rdi, 1
-        mov rsi, msg
-        mov rdi, 4
-        syscall
+    mov rax, 60
+    xor rdi, rdi
+    xor rsi, rsi
+    syscall
 
-        mov rax, 60
-        mov rdi, 0
-        syscall
+not_equal:
+    xor rax, rax
+    xor rdi, rdi
 
-not_42:
-	xor rax, rax
-	xor rdi, rdi	
-
-        mov rax, 60
-        mov rdi, 1
-        syscall
+    mov rax, 60
+    mov rdi, 1
+    syscall
